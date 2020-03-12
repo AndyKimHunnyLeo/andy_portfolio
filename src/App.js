ca62fa3ccf97f632f248321bbf3925526f8c9119
Andy Kim <hyungSeok.kim@outlook.com>
2020-03-13 12:04:06 +1300

    built structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Route, BrowserRouter } from 'react-router-dom'
+import './App.css';
+import Navigation from './components/Navigation';
+import home from './routes/home';
+import projects from './routes/projects';
+import contact from './routes/contacts';
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <span>daoidhioawdoqw</span>
+    <BrowserRouter>
+      <Navigation />
+      <Route path="/projects" component={projects}></Route>
+      <Route path="/" exact={true} component={home}></Route>
+      <Route path="/contact" component={contact}></Route>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-dom';
+import "./Navigation.css"
+
+function Navigation() {
+    return (
+        <div class="nav">
+
+            <Link to="/" target="_top" class="nav_text home">home</Link>
+            <Link to="/projects" target="_top" class="nav_text projects" >projects</Link>
+            <Link to="/contact" target="_top" class="nav_text ">contact</Link>
+
+        </div >
+    );
+}
+
+
+export default Navigation;
